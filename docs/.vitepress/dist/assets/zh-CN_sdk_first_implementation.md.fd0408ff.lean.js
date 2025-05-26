9e5609dc5c79cfd0f20fbcb64bca3ae44bca089d
liulipeng <liulipeng@huan.tv>
2025-05-26 16:47:49 +0800

    docs(docs): build & publish

---
+++ docs/.vitepress/dist/assets/zh-CN_sdk_first_implementation.md.fd0408ff.lean.js
@@ -0,0 +1,7 @@
+import{o as t,c as a,a as n}from"./app.db97d03c.js";const l='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"zh-CN/sdk/first_implementation.md","lastUpdated":1746602666000}',s={},r=Object.assign(s,{__name:"first_implementation",setup(o){return(i,e)=>(t(),a("div",null,e[0]||(e[0]=[n(`<ol><li>\u914D\u7F6E\u9879\u76EErepositories</li></ol><div class="language-groovy"><pre><code> repositories <span class="token punctuation">{</span>
+    maven <span class="token punctuation">{</span> url <span class="token string">&#39;https://nexus.extscreen.com/repository/maven-public/&#39;</span> <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre></div><ol start="2"><li>\u5F15\u5165\u5230\u9879\u76EE\u4E2D</li></ol><div class="language-groovy"><pre><code>dependencies <span class="token punctuation">{</span>
+    implementation <span class="token string">&#39;com.extscreen.runtime:official:\u7248\u672C\u53F7&#39;</span>
+<span class="token punctuation">}</span>
+</code></pre></div><p><img src="https://img.shields.io/badge/dynamic/xml?url=https://nexus.extscreen.com/repository/maven-releases/com/extscreen/runtime/official/maven-metadata.xml&amp;query=metadata/versioning/latest&amp;label=Latest" alt=""></p>`,5)])))}});export{l as __pageData,r as default};
