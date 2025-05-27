4e5f2f51328a6f666b77768ffab4976a84ac501b
liulipeng <liulipeng@huan.tv>
2025-05-27 15:53:03 +0800

    docs(components): guide docs

---
+++ docs/examples/guide/router/app.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="root">
+    <es-router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core'
+import { useESRouterManager } from '@extscreen/es3-core'
+import { useESRouter } from '@extscreen/es3-router'
+import type { ESAppParams } from '@extscreen/es3-core'
+import type { ESApp } from '@extscreen/es3-vue'
+
+export default defineComponent({
+  name: 'App',
+  emits: [],
+  setup() {
+    const router = useESRouter()
+
+    //控制是否自动跳转到首页
+    const routerManager = useESRouterManager()
+    routerManager.setAutoRedirectEnabled(false)
+
+    function onESCreate(app: ESApp, params: ESAppParams) {
+      //
+    }
+
+    function onESCreated(success: boolean) {
+      //初始化完毕，进行跳转
+      router.push({
+        name: 'index',
+        params: {},
+      })
+    }
+
+    return {
+      onESCreate,
+      onESCreated,
+    }
+  },
+})
+</script>
+
+<style scoped>
+#root {
+  width: 1920px;
+  height: 1080px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- docs/zh-CN/guide/router/entry.md
+++ docs/zh-CN/guide/router/entry.md
@@ -5,7 +5,11 @@ lang: zh-CN
 
 # 应用入口
 
-应用的入口需要配置在`RouterOptions`中。以`main`字段为key，入口页面路径为value。 例如：在`RouterOptions`中设置应用入口页面为`index`。
+## 自动打开入口页面（默认）
+
+应用的入口需要配置在`RouterOptions`中。以`main`字段为key，入口页面路径为value。
+
+例如：在`RouterOptions`中设置应用入口页面为`index`。应用启动后自动打开`index`页面。
 
 ```ts
 
@@ -23,3 +27,34 @@ const app: ESApp = createESApp(application, router);
 app.use(install);
 
 ```
+
+## 手动打开入口页面
+
+### 第一步、关闭自动跳转页面
+
+在`app.vue`的`setup`中调用：
+
+```ts
+import { useESRouterManager } from "@extscreen/es3-core";
+
+const routerManager = useESRouterManager();
+routerManager.setAutoRedirectEnabled(false);
+```
+
+### 第二步、手动跳转页面
+
+在`app.vue`的声明周期方法`onESCreated`中调用：
+
+```ts
+const router = useESRouter();
+
+router.push({
+  name: '需要跳转的页面路由名称',
+  params: {},//需要跳转的页面路由参数
+});
+```
+:::demo ## 完整示例
+
+guide/router/app
+
+:::
