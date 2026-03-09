97132e710bd0c6fd81dd75dfdbf3cd4c74a22876
liulipeng <pengliliu8086@163.com>
2026-03-09 12:31:24 +0800

    docs: ai docs

--- docs/.vitepress/dist/assets/index.md.563479f7.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:docs/.vitepress/dist/assets/index.md.e9393196.js
-import{o as e,c as t}from"./app.68b1ad6d.js";const _='{"title":"A Vue 3 UI Framework","description":"","frontmatter":{"title":"A Vue 3 UI Framework","lang":"zh-CN","page":true},"headers":[],"relativePath":"index.md","lastUpdated":1746602666000}',a={},c=Object.assign(a,{__name:"index",setup(r){return(n,o)=>(e(),t("div"))}});export{_ as __pageData,c as default};
-========
-import{o as e,c as t}from"./app.b73e02ce.js";const _='{"title":"A Vue 3 UI Framework","description":"","frontmatter":{"title":"A Vue 3 UI Framework","lang":"zh-CN","page":true},"headers":[],"relativePath":"index.md","lastUpdated":1746602666000}',a={},c=Object.assign(a,{__name:"index",setup(r){return(n,o)=>(e(),t("div"))}});export{_ as __pageData,c as default};
->>>>>>>> 49a7dbe8d7ae66f956f23862ae3abd7565e466dc:docs/.vitepress/dist/assets/index.md.563479f7.js
--- docs/.vitepress/dist/assets/zh-CN_sdk_empty.md.6e372f3d.lean.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:docs/.vitepress/dist/assets/zh-CN_sdk_empty.md.6618b090.lean.js
-import{o as t,c as e}from"./app.68b1ad6d.js";const o='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"zh-CN/sdk/empty.md","lastUpdated":1746602666000}',a={},n=Object.assign(a,{__name:"empty",setup(s){return(r,_)=>(t(),e("div"))}});export{o as __pageData,n as default};
-========
-import{o as t,c as e}from"./app.b73e02ce.js";const o='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"zh-CN/sdk/empty.md","lastUpdated":1746602666000}',a={},n=Object.assign(a,{__name:"empty",setup(s){return(r,_)=>(t(),e("div"))}});export{o as __pageData,n as default};
->>>>>>>> 49a7dbe8d7ae66f956f23862ae3abd7565e466dc:docs/.vitepress/dist/assets/zh-CN_sdk_empty.md.6e372f3d.lean.js
